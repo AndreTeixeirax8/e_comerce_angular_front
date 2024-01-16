eee63b25bf5ef0ef46963c5e5fcd41821e90ff25
André Teixeira <andre-x8@hotmail.com>
2024-01-16 16:23:26 -0300

    criado componente de produto lista

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
-
-
+import { ProductRoutingModule } from './product-routing.module';
 
 @NgModule({
-  declarations: [
-    ProductListComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ProductListComponent],
+  imports: [CommonModule, ProductRoutingModule],
 })
-export class ProductModule { }
+export class ProductModule {}
