cd0a0ba8e5321e7bbb48aaa995e94f5119e9d8e0
André Teixeira <andre-x8@hotmail.com>
2024-01-17 06:58:28 -0300

    criado a conexão com back em produtos

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
+  constructor(public productService: ProductService) {}
 
   ngOnInit(): void {
+    this.loadProducts();
   }
+  /*
+  loadProducts() {
+    return this.productService
+      .getProducts()
+      .subscribe((data: any) => console.log('data: ' + data));
+  }*/
 
+  loadProducts() {
+    return this.productService.getProducts().subscribe((data: any) => {
+      console.log('data:', data); // Exibe o objeto no formato de string
+      console.dir(data); // Exibe o objeto como uma árvore
+    });
+  }
 }
