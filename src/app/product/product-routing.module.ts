9c86a00dca6722cba74a06e80d577e9b01fd9a90
André Teixeira <andre-x8@hotmail.com>
2024-01-23 12:18:04 -0300

    alteracao de nome de arquivo

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductHomeComponent } from './product-home/product-home.component';
 
 const routes: Routes = [
   {
     path: 'list',
-    component: ProductListComponent,
+    component: ProductHomeComponent,
   },
 ];
 
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductHomeComponent } from './product-home/product-home.component';
 import { ProductRoutingModule } from './product-routing.module';
 
 @NgModule({
-  declarations: [ProductListComponent],
+  declarations: [ProductHomeComponent],
   imports: [CommonModule, ProductRoutingModule],
 })
 export class ProductModule {}
