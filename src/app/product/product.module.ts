9abe69babe7e8016eb3a21f9ccc9cce193c166b3
André Teixeira <andre-x8@hotmail.com>
2024-01-25 17:37:01 -0300

    adicionado detalhes do produto

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ProductHomeComponent } from './product-home/product-home.component';
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductBagsComponent } from './product-bags/product-bags.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
-  declarations: [ProductHomeComponent, ProductBagsComponent],
+  declarations: [ProductHomeComponent, ProductBagsComponent, ProductDetailComponent],
   imports: [CommonModule, ProductRoutingModule],
 })
 export class ProductModule {}
