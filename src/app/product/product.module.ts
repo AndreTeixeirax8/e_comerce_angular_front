9b30b155523161ba2a5fde10bb2fb47d71587407
André Teixeira <andre-x8@hotmail.com>
2024-01-29 20:21:06 -0300

    adicionado modulo de subcategoria

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -4,9 +4,10 @@ import { ProductHomeComponent } from './product-home/product-home.component';
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductBagsComponent } from './product-bags/product-bags.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { SubCategoryComponent } from './sub-category/sub-category.component';
 
 @NgModule({
-  declarations: [ProductHomeComponent, ProductBagsComponent, ProductDetailComponent],
+  declarations: [ProductHomeComponent, ProductBagsComponent, ProductDetailComponent, SubCategoryComponent],
   imports: [CommonModule, ProductRoutingModule],
 })
 export class ProductModule {}
