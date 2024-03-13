bc497c03dc35e43bbca7bbdb967a1deee89dc66f
André Teixeira <andre-x8@hotmail.com>
2024-03-13 20:33:28 -0300

    iniciado a parte de update

--- src/app/product/product-edit/product-edit.component.ts
+++ src/app/product/product-edit/product-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { ProductService } from '../services/product.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-product-edit',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductEditComponent implements OnInit {
 
-  constructor() { }
+  productForm:any
 
+  constructor(
+    private activatedRoute:ActivatedRoute,
+    private productService:ProductService,
+    private formBuilder:FormBuilder,
+    private router:Router,
+  ) { }
+
+  //PAREI EM 19:00
   ngOnInit(): void {
+    this.productForm =this.formBuilder.group({
+      title:[''],
+      description:[''],
+      imgUrl1:[''],
+      price:[''],
+      quantity:[''],
+      size:[''],
+      color:[''],
+      shippings:[''],
+      sex:[''],
+      brands:[''],
+      category:[''],
+      subcategory:[''],
+    })
+  }
+
+  onSubmit()
+  {
+
   }
 
 }
