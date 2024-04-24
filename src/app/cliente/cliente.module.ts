d10685d113c64b9294c90e511f2d22fff6f1587f
André Teixeira <andre-x8@hotmail.com>
2024-04-24 19:09:34 -0300

    ajuste em cliente

--- src/app/cliente/cliente.module.ts
+++ src/app/cliente/cliente.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from '../product/shared/navbar/navbar.component';
+import { NavbarComponent } from 'src/app/product/shared/navbar';
 
 
 @NgModule({
