c64ae6dc90a72c70d1e6f4c1899ad707b42e088f
André Teixeira <andre-x8@hotmail.com>
2024-04-25 20:17:32 -0300

    adicionado a importacao por barril

--- src/app/cliente/cliente.module.ts
+++ src/app/cliente/cliente.module.ts
@@ -1,28 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClienteCreateComponent } from './cria-cliente/cliente-create.component';
-import { ClienteRoutingModule } from './cliente-routing.module';
+import { ClienteRoutingModule } from 'src/app/cliente';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
-// Importe o módulo onde o NavbarComponent está declarado
 import { ProductModule } from '../product/product.module'; 
 
 @NgModule({
   declarations: [    
    ClienteCreateComponent,
-   // O NavbarComponent está declarado no ProductModule
-   // Portanto, não é necessário declará-lo aqui
+   
   ],
   imports: [
     CommonModule,
     ClienteRoutingModule,
     ReactiveFormsModule, 
     RouterModule, 
-    // Importe o ProductModule
     ProductModule
   
   ],
-  // Remova a exportação do NavbarComponent
+  
 })
 export class ClienteModule {}
