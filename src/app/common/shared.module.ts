d0b6fd04e1736c98423bcb90b46a06bb0c548000
André Teixeira <andre-x8@hotmail.com>
2024-05-18 12:04:49 -0300

    corrigido bug das rotas menu

--- src/app/common/shared.module.ts
+++ src/app/common/shared.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Navbar2Component } from './navbar2/navbar2.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [Navbar2Component],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule, 
+  ],
   exports: [Navbar2Component]
 })
 export class SharedModule { }
